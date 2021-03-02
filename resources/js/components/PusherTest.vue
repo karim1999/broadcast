39e60caf27141784bdccc84311e11c9372d25a5d
Karim Abdelgwad <karim.elbadry2@gmail.com>
2021-03-02 03:10:11 +0200

    turn

--- resources/js/components/PusherTest.vue
+++ resources/js/components/PusherTest.vue
@@ -42,14 +42,40 @@ import 'vue-loading-overlay/dist/vue-loading.css';
                 isBlurred: false,
                 peerConfig: {
                     iceServers: [
+                        {url:'stun:stun01.sipphone.com'},
+                        {url:'stun:stun.ekiga.net'},
+                        {url:'stun:stun.fwdnet.net'},
+                        {url:'stun:stun.ideasip.com'},
+                        {url:'stun:stun.iptel.org'},
+                        {url:'stun:stun.rixtelecom.se'},
+                        {url:'stun:stun.schlund.de'},
+                        {url:'stun:stun.l.google.com:19302'},
+                        {url:'stun:stun1.l.google.com:19302'},
+                        {url:'stun:stun2.l.google.com:19302'},
+                        {url:'stun:stun3.l.google.com:19302'},
+                        {url:'stun:stun4.l.google.com:19302'},
+                        {url:'stun:stunserver.org'},
+                        {url:'stun:stun.softjoys.com'},
+                        {url:'stun:stun.voiparound.com'},
+                        {url:'stun:stun.voipbuster.com'},
+                        {url:'stun:stun.voipstunt.com'},
+                        {url:'stun:stun.voxgratia.org'},
+                        {url:'stun:stun.xten.com'},
                         {
-                            "urls": [
-                                "turn:13.250.13.83:3478?transport=udp"
-                            ],
-                            "username": "YzYNCouZM1mhqhmseWk6",
-                            "credential": "YzYNCouZM1mhqhmseWk6"
-                        }
-                    ]
+                            url: 'turn:numb.viagenie.ca',
+                            credential: 'muazkh',
+                            username: 'webrtc@live.com'
+                        },
+                        {
+                            url: 'turn:192.158.29.39:3478?transport=udp',
+                            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                            username: '28224511:1379330808'
+                        },
+                        {
+                            url: 'turn:192.158.29.39:3478?transport=tcp',
+                            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                            username: '28224511:1379330808'
+                        }                    ]
                 },
             }
         },
