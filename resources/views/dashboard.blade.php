127cc506dca57034a370b1f524f09222f2d96e2d
Karim Abdelgwad <karim.elbadry2@gmail.com>
2021-03-02 04:01:51 +0200

    full width

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,7 +24,7 @@
 {{--        </div>--}}
 {{--    </div>--}}
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div id="app" class="p-6 bg-white border-b border-gray-200">
                     @isset ($is_admin)
