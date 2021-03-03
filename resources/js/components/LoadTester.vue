1625ea47e450c0c38bed8d0e14a0f423a33f8e01
Karim Abdelgwad <karim.elbadry2@gmail.com>
2021-03-03 13:37:06 +0200

    10

--- resources/js/components/LoadTester.vue
+++ resources/js/components/LoadTester.vue
@@ -36,7 +36,7 @@ export default {
     },
     data: function (){
         return {
-            peersNum: 7,
+            peersNum: 10,
             peers: [],
             isLoading: false,
             channel: null,
