b378dfd36fa1f51cbf498cb0181c1f9775af1bfd
Karim Abdelgwad <karim.elbadry2@gmail.com>
2021-03-04 22:09:20 +0200

    https

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,11 +39,9 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    // forceTLS: true,
-    encrypted: true,
 
     //Laravel Websockets
-    wsHost: "cloudgear.dev",
+    wsHost: "127.0.0.1",
     wsPort: 6001,
     wssPort: 6001,
     forceTLS: true,
